9718fc0f56ec5f0da58382a9a5cb86143c8cab80
Brendan Lewis <brendanelewis@gmail.com>
2021-07-26 11:21:26 -0400

    Feat: render test object to see camera movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "../external/ChiraEngine/src/core/engine.h"
 #include "../external/ChiraEngine/src/render/freecam.h"
+#include "../external/ChiraEngine/src/loader/objMeshLoader.h"
 
 int main() {
     engine engine;
     virtualFileSystem::addResourceDirectory("resources/editor/");
     engine.getSettingsLoader()->setValue("engine", "title", std::string("Chira Editor"), true, true);
+    objMeshLoader meshLoader;
 
     freecam player{&engine}; // todo: make own camera class
     engine.setCamera(&player);
@@ -15,11 +17,18 @@ int main() {
     }));
 #endif
 
+    engine.addShader("unlit", new shader("unlit.vsh", "unlit.fsh"));
+    engine.addMesh("teapot", new mesh(&meshLoader, "teapot.obj"));
+
     engine.addInitFunction([](class engine* e) {
         engine::setBackgroundColor(0.9098f, 0.9137f, 0.9098f, 1.0f);
         e->captureMouse(false);
     });
     engine.init();
+
+    engine.addRenderFunction([](class engine* e) {
+        e->getMesh("teapot")->render(e->getShader("unlit"));
+    });
     engine.run();
     return 0;
 }
