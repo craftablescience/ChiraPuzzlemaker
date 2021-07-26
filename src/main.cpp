879a4b9a40d24ea1d7bcd944b97cb7db81d10a2b
Brendan Lewis <brendanelewis@gmail.com>
2021-07-26 08:50:49 -0400

    Feat: create engine window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "../external/ChiraEngine/src/core/engine.h"
+#include "../external/ChiraEngine/src/render/freecam.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    engine engine;
+    virtualFileSystem::addResourceDirectory("resources/editor/");
+    engine.getSettingsLoader()->setValue("engine", "title", std::string("Chira Editor"), true, true);
+
+    freecam player{&engine}; // todo: make own camera class
+    engine.setCamera(&player);
+
+#if DEBUG
+    engine.addKeybind(keybind(GLFW_KEY_ESCAPE, GLFW_PRESS, [](class engine* e) {
+        e->stop();
+    }));
+#endif
+
+    engine.addInitFunction([&player](class engine* e) {
+        engine::setBackgroundColor(0.9098f, 0.9137f, 0.9098f, 1.0f);
+        e->captureMouse(false);
+        player.init(e);
+    });
+    engine.init();
+    engine.run();
     return 0;
 }
