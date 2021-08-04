a24facd1e84187ce59615d785818bec6280ac563
Brendan Lewis <brendanelewis@gmail.com>
2021-08-04 08:02:10 -0400

    Feat: use light themed GUI

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,89 @@
 #include "../external/ChiraEngine/src/render/texture2d.h"
 #include "loader/vtfImage.h"
 
+// https://github.com/ocornut/imgui/issues/707#issuecomment-362574409
+void SetupImGuiStyle() {
+    /*
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->Clear();
+    ImFont* font = io.Fonts->AddFontFromFileTTF(R"(C:\Windows\Fonts\segoeui.ttf)", 18.0f);
+    if (font != nullptr) {
+        io.FontDefault = font;
+    } else {
+        io.Fonts->AddFontDefault();
+    }
+    io.Fonts->Build();
+     */
+    ImGuiStyle* style = &ImGui::GetStyle();
+    float hspacing = 8.0;
+    float vspacing = 6.0;
+    style->DisplaySafeAreaPadding = ImVec2(0, 0);
+    style->WindowPadding = ImVec2(hspacing/2, vspacing);
+    style->FramePadding = ImVec2(hspacing, vspacing);
+    style->ItemSpacing = ImVec2(hspacing, vspacing);
+    style->ItemInnerSpacing = ImVec2(hspacing, vspacing);
+    style->IndentSpacing = 20.0f;
+
+    style->WindowRounding = 0.0f;
+    style->FrameRounding = 0.0f;
+
+    style->WindowBorderSize = 0.0f;
+    style->FrameBorderSize = 1.0f;
+    style->PopupBorderSize = 1.0f;
+
+    style->ScrollbarSize = 20.0f;
+    style->ScrollbarRounding = 0.0f;
+    style->GrabMinSize = 5.0f;
+    style->GrabRounding = 0.0f;
+
+    ImVec4 white = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+    ImVec4 transparent = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    ImVec4 dark = ImVec4(0.00f, 0.00f, 0.00f, 0.20f);
+    ImVec4 darker = ImVec4(0.00f, 0.00f, 0.00f, 0.50f);
+
+    ImVec4 background = ImVec4(0.95f, 0.95f, 0.95f, 1.00f);
+    ImVec4 text = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+    ImVec4 border = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+    ImVec4 grab = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
+    ImVec4 header = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+    ImVec4 active = ImVec4(0.00f, 0.47f, 0.84f, 1.00f);
+    ImVec4 hover = ImVec4(0.00f, 0.47f, 0.84f, 0.20f);
+
+    style->Colors[ImGuiCol_Text] = text;
+    style->Colors[ImGuiCol_WindowBg] = background;
+    style->Colors[ImGuiCol_ChildBg] = background;
+    style->Colors[ImGuiCol_PopupBg] = white;
+
+    style->Colors[ImGuiCol_Border] = border;
+    style->Colors[ImGuiCol_BorderShadow] = transparent;
+
+    style->Colors[ImGuiCol_Button] = header;
+    style->Colors[ImGuiCol_ButtonHovered] = hover;
+    style->Colors[ImGuiCol_ButtonActive] = active;
+
+    style->Colors[ImGuiCol_FrameBg] = white;
+    style->Colors[ImGuiCol_FrameBgHovered] = hover;
+    style->Colors[ImGuiCol_FrameBgActive] = active;
+
+    style->Colors[ImGuiCol_MenuBarBg] = header;
+    style->Colors[ImGuiCol_Header] = header;
+    style->Colors[ImGuiCol_HeaderHovered] = hover;
+    style->Colors[ImGuiCol_HeaderActive] = active;
+
+    style->Colors[ImGuiCol_CheckMark] = text;
+    style->Colors[ImGuiCol_SliderGrab] = grab;
+    style->Colors[ImGuiCol_SliderGrabActive] = darker;
+
+    //style->Colors[ImGuiCol_CloseButton] = transparent;
+    //style->Colors[ImGuiCol_CloseButtonHovered] = transparent;
+    //style->Colors[ImGuiCol_CloseButtonActive] = transparent;
+
+    style->Colors[ImGuiCol_ScrollbarBg] = header;
+    style->Colors[ImGuiCol_ScrollbarGrab] = grab;
+    style->Colors[ImGuiCol_ScrollbarGrabHovered] = dark;
+    style->Colors[ImGuiCol_ScrollbarGrabActive] = darker;
+}
+
 int main() {
     engine engine;
     virtualFileSystem::addResourceDirectory("resources/editor/");
@@ -34,6 +117,7 @@ int main() {
     engine.init();
 
     engine.addRenderFunction([](class engine* e) {
+        SetupImGuiStyle();
         if (ImGui::BeginMainMenuBar()) {
             if (ImGui::BeginMenu("File")) {
                 if (ImGui::MenuItem("New")) {
